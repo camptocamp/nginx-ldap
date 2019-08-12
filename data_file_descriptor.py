9aafc73ae9882302925b6c344bdab330af721036
Pierre Mellier <pierre.mellier@epfl.ch>
2019-08-12 08:51:18 +0200

    add string representation

--- data_file_descriptor.py
+++ data_file_descriptor.py
@@ -5,6 +5,7 @@ class AccessType(Enum):
     public = 1
     free = 2
 
+DEBUG = False
 
 class DataFileDescriptor():
     def __init__(self, file_path):
@@ -14,13 +15,20 @@ class DataFileDescriptor():
         self.access_type = AccessType.private
 
     def add_user(self, user):
-        self.allowed_users.add(user)
+        if user is not None :
+            self.allowed_users.add(user)
 
     def add_group(self, group):
-        self.allowed_groups.add(group)
+        if group is not None :
+            self.allowed_groups.add(group)
 
     def set_access_type(self, access_type):
         self.access_type = access_type
+
+    def __str__(self):
+        return "%pth->"+self.file_path+"%grps->"+str(self.allowed_groups)+"%usrs->"+str(self.allowed_users)+"%acct->"+str(self.access_type)+"\n"
+
+    __repr__ = __str__
 
 class DataFileStorage():
     def __init__(self):
@@ -33,12 +41,20 @@ class DataFileStorage():
         return self.storage.get(key, "")
 
     def save(self, filename):
+        if DEBUG:
+            for key,value in self.storage.items() :
+                print("{key}:{value}\n".format(key=str(key), value=str(value)))
         with open(filename, 'wb') as f:
             pickle.dump(self.storage, f, pickle.HIGHEST_PROTOCOL)
 
     def load(self, filename):
         with open(filename, 'rb') as f:
             self.storage = pickle.load(f)
+
+    def __str__(self):
+        return str(self.storage)
+
+    __repr__ = __str__
 
 
 
